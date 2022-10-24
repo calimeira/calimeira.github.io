5ddd22c95dfe6f1ba781f936aed1c0f07a5a43b8
Carlos <meiracali95@gmail.com>
2022-10-24 19:21:40 -0300

    act

--- mint/src/enviroments/enviroment.js
+++
@@ -1,9 +0,0 @@
-export const environment = {
-    application:
-    {
-      name: 'react-startup',
-      version: 'React 18.2.0',
-      bootstrap: 'Bootstrap 5.1.3',
-      fontawesome: 'Font Awesome 6.1.1',
-    }
-  };
